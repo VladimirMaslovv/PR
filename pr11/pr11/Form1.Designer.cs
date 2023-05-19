396805b170b894450fa93f5d875763795d748ed8
VladimirMaslovv <133869802+VladimirMaslovv@users.noreply.github.com>
2023-05-19 22:44:22 +0300

    Add files via upload

---
+++ pr11/pr11/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace pr11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picFrame = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // picFrame
+            // 
+            this.picFrame.Location = new System.Drawing.Point(80, 100);
+            this.picFrame.Name = "picFrame";
+            this.picFrame.Size = new System.Drawing.Size(300, 300);
+            this.picFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picFrame.TabIndex = 1;
+            this.picFrame.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(164, 24);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(296, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.picFrame);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox picFrame;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ pr11/pr11/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr11
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap[] Frames;
+        private int FrameNum = 0;
+        private int Speed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load();
+        }
+
+        private void Form1_Load()
+        {
+            Frames = new Bitmap[20];
+            for(int i = 0; i< 10; i++)
+            {
+                Frames[i] = new Bitmap($"Frame{i}.png");
+                Frames[19-i] = new Bitmap($"Frame{i}.png");
+            }
+
+            picFrame.Image = Frames[FrameNum];
+        }        
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //picFrame.Image = Frames[trackBar1.Value];
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Speed = 1; 
+
+            if (timer1.Enabled == false) { timer1.Enabled = true; trackBar1.Enabled = false; button1.Text = "Stop"; }
+            else { timer1.Enabled = false; trackBar1.Enabled = true; button1.Text = "Start"; }
+
+            //FrameNum = ++FrameNum; if (FrameNum == 10) FrameNum = 9;
+            //picFrame.Image = Frames[FrameNum];
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (Speed == trackBar1.Value)
+            {
+                Speed = 0;
+                FrameNum = ++FrameNum % Frames.Length;
+                picFrame.Image = Frames[FrameNum];
+            }
+
+            Speed++;
+        }
+    }
+}
